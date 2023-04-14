3bf7447193a491ff8d018d1b6446bdad9d50dfa5
Dan Anderson <70665971+danielmckinnonanderson@users.noreply.github.com>
2023-04-14 12:28:58 -0500

    Update config with setup funcs for Mac & Linux

--- config.lua
+++ config.lua
@@ -8,6 +8,7 @@ lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.terminal.active = false
 lvim.leader = "space"
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+vim.opt.relativenumber = true
 
 
 -- ******
@@ -24,72 +25,92 @@ if lvim.colorscheme == "seoulbones" then
   ]])
 end
 
+
 -- ********************* 
 -- #OS-SPECIFIC-SETTINGS
 -- ********************* 
-local function os_specific_setup(sysname)
 
-  -- Windows
-  local function win_setup()
+-- Windows
+local function win_setup()
 
-    -- Function to use win32yank for clipboard stuff
-    local function win_use_winyank_clipboard()
-      vim.g.clipboard = {
-        copy = {
-          ["+"] = "win32yank.exe -i --crlf",
-          ["*"] = "win32yank.exe -i --crlf",
-        },
-        paste = {
-          ["+"] = "win32yank.exe -o --lf",
-          ["*"] = "win32yank.exe -o --lf",
-        },
-      }
-    end
-
-    -- Function to set Windows Powershell as default 
-    local function win_default_to_powershell()
-      vim.opt.shell = "pwsh.exe -NoLogo"
-      vim.opt.shellcmdflag =
-        "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
-      vim.cmd [[
-          let &shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-          let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-          set shellquote= shellxquote=
-        ]]
-      lvim.builtin.terminal.shell = "pwsh.exe -NoLogo"
-    end
-
-    -- Function to address nvim-tree performance issues on Windows, see kyazdani42/nvim-tree.lua#549
-    local function win_improve_nvimtree_performance()
-      lvim.builtin.nvimtree.setup.diagnostics.enable = nil
-      lvim.builtin.nvimtree.setup.filters.custom = nil
-      lvim.builtin.nvimtree.setup.git.enable = nil
-      lvim.builtin.nvimtree.setup.update_cwd = nil
-      lvim.builtin.nvimtree.setup.update_focused_file.update_cwd = nil
-      lvim.builtin.nvimtree.setup.view.side = "left"
-      lvim.builtin.nvimtree.setup.renderer.highlight_git = nil
-      lvim.builtin.nvimtree.setup.renderer.icons.show.git = nil
-    end
-
-    win_default_to_powershell()
-    win_use_winyank_clipboard()
-    win_improve_nvimtree_performance()
+  -- Function to use win32yank for clipboard stuff
+  local function win_use_winyank_clipboard()
+    vim.g.clipboard = {
+      copy = {
+        ["+"] = "win32yank.exe -i --crlf",
+        ["*"] = "win32yank.exe -i --crlf",
+      },
+      paste = {
+        ["+"] = "win32yank.exe -o --lf",
+        ["*"] = "win32yank.exe -o --lf",
+      },
+    }
   end
 
-  if sysname == "Windows_NT" then
-    win_setup()
-  -- TODO
-  elseif sysname == "Darwin" then
-    print("Hello Mac")
-  -- TODO
-  elseif sysname == "" then
-    print("Hello Linux")
+  -- Function to set Windows Powershell as default 
+  local function win_default_to_powershell()
+    vim.opt.shell = "pwsh.exe -NoLogo"
+    vim.opt.shellcmdflag =
+      "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+    vim.cmd [[
+        let &shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+        let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+        set shellquote= shellxquote=
+      ]]
+    lvim.builtin.terminal.shell = "pwsh.exe -NoLogo"
   end
+
+  -- Function to address nvim-tree performance issues on Windows, see kyazdani42/nvim-tree.lua#549
+  local function win_improve_nvimtree_performance()
+    lvim.builtin.nvimtree.setup.diagnostics.enable = nil
+    lvim.builtin.nvimtree.setup.filters.custom = nil
+    lvim.builtin.nvimtree.setup.git.enable = nil
+    lvim.builtin.nvimtree.setup.update_cwd = nil
+    lvim.builtin.nvimtree.setup.update_focused_file.update_cwd = nil
+    lvim.builtin.nvimtree.setup.view.side = "left"
+    lvim.builtin.nvimtree.setup.renderer.highlight_git = nil
+    lvim.builtin.nvimtree.setup.renderer.icons.show.git = nil
+  end
+
+  win_default_to_powershell()
+  win_use_winyank_clipboard()
+  win_improve_nvimtree_performance()
+end
+
+-- Nvim tree standard setup for use with Mac & Linux
+local function unix_nvim_tree_setup()
+  lvim.builtin.nvimtree.setup.view.side = "left"
+  lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+end
+
+-- Linux
+local function linux_setup()
+  -- Fix weird visual bug causing buffer title to appear in line constantly
+  vim.opt.title = false
+
+  -- Only use transparent background plugin on Linux
+  require("transparent").setup({
+    enable = true,
+    extra_groups = {},
+    exclude = {},
+  })
+
+  unix_nvim_tree_setup()
+end
+
+local function mac_setup()
+  unix_nvim_tree_setup()
 end
 
 local sysname = vim.loop.os_uname().sysname
-os_specific_setup(sysname)
 
+if sysname == "Windows_NT" then
+  win_setup()
+elseif sysname == "Darwin" then
+  mac_setup()
+elseif sysname == "Linux" then
+  linux_setup()
+end
 
 -- ***********
 -- #TREESITTER
@@ -114,6 +135,10 @@ lvim.builtin.treesitter.highlight.enable = true
 -- #PLUGINS
 -- ******** 
 lvim.plugins = {
+  -- Background opacity
+  "xiyaowong/nvim-transparent",
+  -- Themes
+  "ellisonleao/gruvbox.nvim",
   {
     "mcchrish/zenbones.nvim",
     requires = "rktjmp/lush.nvim"
@@ -123,3 +148,4 @@ lvim.plugins = {
     requires = "rktjmp/lush.nvim"
   },
 }
+
